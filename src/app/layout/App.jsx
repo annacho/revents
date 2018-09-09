ccb5456cd62542ea4e8aa24b4b9afadb3c7518b7
annacho <annacho13@gmail.com>
2018-09-09 12:20:36 -0600

    Add routing

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -1,16 +1,41 @@
 import React, { Component } from 'react';
 import { Container } from 'semantic-ui-react';
-import EventDashboard from '../../features/event/EventDashboard/EventDashboard.jsx';
+import { Route, Switch } from 'react-router-dom';
+import EventDashboard from '../../features/events/EventDashboard/EventDashboard.jsx';
 import NavBar from '../../features/nav/NavBar/NavBar.jsx';
+import EventForm from '../../features/events/EventForm/EventForm.jsx';
+import SettingsDashboard from '../../features/user/Settings/SettingsDashboard.jsx';
+import UserDetailPage from '../../features/user/UserDetail/UserDetailPage.jsx';
+import PeopleDashboard from '../../features/user/PeopleDashboard/PeopleDashboard.jsx';
+import EventDetailPage from '../../features/events/EventDetail/EventDetailPage.jsx';
+import HomePage from '../../features/home/HomePage.jsx';
 
 class App extends Component {
   render() {
     return (
       <div>
-        <NavBar/>
-        <Container className="main">
-          <EventDashboard/>
-        </Container>
+        <Switch>
+          <Route exact path='/' component={HomePage}/>
+        </Switch>
+
+        <Route
+          path="/(.+)"
+          render={() => (
+            <div>
+              <NavBar/>
+              <Container className="main">
+                <Switch>
+                  <Route path='/events' component={EventDashboard}/>
+                  <Route path='/event/:id' component={EventDetailPage}/>
+                  <Route path='/people' component={PeopleDashboard}/>
+                  <Route path='/profile/:id' component={UserDetailPage}/>
+                  <Route path='/settings' component={SettingsDashboard}/>
+                  <Route path='/createEvent' component={EventForm}/>
+                </Switch>
+              </Container>
+            </div>
+          )}
+        />
       </div>
     );
   }
